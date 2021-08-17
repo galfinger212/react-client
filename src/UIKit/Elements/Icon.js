b4bb2115e7ea8a33d03f33e540485e67bed50ffe
galfinger212 <galfinger1@gmail.com>
2021-08-17 09:31:59 +0300

    sendSeen?.sender chat

--- src/UIKit/Elements/Icon.js
+++
@@ -1,9 +0,0 @@
-const Icon = (props) => {
-    return (
-        <div data-testid="IconDiv" className="Icon">
-            <i data-testid="IconImageDiv" className={`${props.i ? `fas fa-${props.i}` : ""}`}></i>
-        </div>
-    );
-}
-
-export default Icon;
